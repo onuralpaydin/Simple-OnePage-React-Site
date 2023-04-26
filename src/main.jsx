7541e119f1fea518820b2c1890593e731de8220f
Onur Alp Aydın <o.a.aydin17@gmail.com>
2023-04-26 15:35:50 +0300

    scss kullanımı eklendi.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './assets/styles/index.css'
-import './assets/styles/reset.css'
+// import './assets/styles/index.css'
+// import './assets/styles/reset.css'
+
+// 1)proje içinde npm install sass
+// 2)scss uzantılı dosya açılması main.jsx içinde import edilmesi.
+// 3)index.css içerisindeki kodların index.scss e aktarılması kodların scss e göre düzenlenmesi
+import "./assets/styles/index.scss"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
